339b5d66820eea11fc1c6d76fe836808bcebcb8a
DUC NGUYEN <ngocduc6991@gmail.com>
2023-07-18 00:30:18 +0900

    first commit

---
+++ src/Output_demo.java
@@ -0,0 +1,44 @@
+import javafx.scene.control.TableColumn;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Output_demo {
+    public static void main(String[] args) {
+//        String studentId ="SV001";
+//        System.out.println("Gia tri studentId la: "+ studentId);
+//        String studentName ="Nguyen van A";
+//        System.out.print("studentName : " + studentName);
+//        int age=25;
+//        System.out.println();
+//        System.out.printf("ma sv : %s; ten sinh vien: %s; tuoi sinh vien: %d",studentId,studentName,age);
+//bai tap 1
+//        String bookId ="B001";
+//        String bookName="Dac Nhan Tam";
+//        float priceIn=50.000F;
+//        float priceOut=60.000F;
+//        String author="Nguyen America";
+//        boolean status=true;
+//        System.out.println();
+//        System.out.println("ma sach la: "+ bookId);
+//        System.out.print("ten sach la: " + bookName);
+//        System.out.printf("\n");
+//        System.out.printf("gia nhap: %f \n gia xuat: %f \n tac gia: %s \n trang thai: %b\n",priceIn,priceOut,author,status);
+//        System.err.println("co loi");
+//        input_demo
+        Scanner scanner = new Scanner(System.in);
+//        System.out.println("nhap ma sv" );
+//        String studentId = scaner.nextLine();
+//        System.out.println("id sv"+ studentId);
+//
+//        System.out.println("nhap ten sv");
+//        String studentName = scaner.nextLine();
+//
+//        System.out.println("ten sv"+ studentName);
+        System.out.println("nhap vao 1 ten bat ki: ");
+        String name= scanner.nextLine();
+        System.out.println("hello : "+ name);
+
+
+    }
+}
---
+++ src/VariableDemo.java
@@ -0,0 +1,19 @@
+public class VariableDemo {
+    public static void main(String[] args) {
+        // Syntax: datatype variableName
+        // khai báo biến chứa thông tin mã sinh viên và tên sinh viên
+        String studentId;
+        String studentName;
+        // syntax: variableName=value
+        // khởi tạo giá trị cho các biến mã sv là sv001 và tên sinh viên là nguyen van a
+        studentId = "SV001";
+        studentName = "Nguyễn Văn A";
+        //format code ctrl + alt L
+        short age = 18;
+        float toan = 7.5F, ly = 9, hoa = 5;
+        int a = 10;
+        float b = 8.3f;
+
+
+    }
+}
